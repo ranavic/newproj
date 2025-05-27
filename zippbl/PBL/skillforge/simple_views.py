1ac70915f7205dfe495a0adb1035cca4c675ef1b
ranavic <ambasthasubham@gmail.com>
2025-05-27 12:27:06 +0530

    folder deleted

--- zippbl/PBL/skillforge/simple_views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def hello_world(request):
-    """Simple function-based view to verify basic Django functionality"""
-    return HttpResponse("Hello, World! SkillForge is working at a basic level.")
--- zippbl/PBL/skillforge/test_views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def test_view(request):
-    """A simple test view to check if basic routing works."""
-    return HttpResponse("SkillForge basic test view is working!")
